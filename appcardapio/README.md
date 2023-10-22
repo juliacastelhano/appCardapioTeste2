fb533ea5886499f82f15281e2175188d6ae4087d
Julia Castelhano <juliacastelhano@hotmail.com>
2023-10-22 15:05:13 -0300

    testando

---
+++ appcardapio/README.md
@@ -0,0 +1,5 @@
+# AppCardapio
+
+```
+git clone https://gitlab.com/gilbriatore/2023/appsm/appcardapio.git
+```
---
+++ appcardapio/app/src/main/java/br/edu/up/app/data/Produto.kt
@@ -0,0 +1,18 @@
+package br.edu.up.app.data
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "produtos")
+data class Produto(
+    @PrimaryKey(autoGenerate = true)
+    var id: Int,
+    var nome: String,
+    var descricao: String = String(),
+    var preco: Double = 0.0,
+    var peso: Int = 0,
+    var foto: String = "semfoto.jpg",
+    var categoria: Int = 0
+) {
+    constructor() : this(0,"","",0.0,0,"semfoto.jpg",0)
+}
